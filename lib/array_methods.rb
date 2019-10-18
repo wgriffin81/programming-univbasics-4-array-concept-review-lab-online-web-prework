9f857b806d3226904ae97a3bb38a972601a4c6d3
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-18 01:44:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,8 +4,8 @@ def find_element_index(array, value_to_find)
   while counter < array.length 
     if array[counter] == value_to_find
       return counter
-      counter +=1
     end
+    counter +=1
   end
 end
 
